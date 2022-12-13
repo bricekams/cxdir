71bd65b34a6ee16a8254b2b4abaf5dba0f948d89
Jotter Kain <jotterkain@gmail.com>
2022-12-14 00:03:25 +0100

    added some known errors

--- errors.sh
+++ errors.sh
@@ -1,9 +1,34 @@
-# More than 2 args to the pathx command when using `pathx create`
-function pathx_create_error_1() {
-    echo -e $red"Error:"$defaultColor \"${args_array[2]}\" "is an invalid argument"
+function pathx__error_unknow-arg () {
+    echo -e $red"pathx:"$defaultColor "\"$1\"" "is invalid"
+}
+
+function pathx__error_invalid_option () {
+    echo -e $red"pathx:"$defaultColor "\"$1\"" "is an invalid option"
+}
+
+function pathx__tips_show_1 () {    
     echo -e $blue"Tips:"$defaultColor "Use"$cyan "pathx create [shortcut name]"$defaultColor "to create a new shortcut pointing to the current directory"
 }
 
-function pathx_unknow_args () {
-    echo "Error:" "\"$1\"" "is invalid" 
+function pathx__error_path_not_provided () {
+    echo -e $red"pathx:"$defaultColor "When using -p or --path, a valid path sould be given"
+    see_help_i_create
+}
+
+function see_help_i_create () {
+    echo -e $blue"Tips:"$defaultColor "Use$cyan pathx create --help$defaultColor to get more info about the \`create\` command"
+}
+
+function see_help_i () {
+    echo -e $blue"Tips:"$defaultColor "See$cyan pathx --help$defaultColor for more info" 
+}
+
+function pathx__error_unknow-command () {
+    echo -e $red"pathx:"$defaultColor "Unknow command $command"
+    see_help_i
+}
+
+function pathx__error_no_command(){
+    echo "pathx: A command must be given"
+    see_help_i
 }
