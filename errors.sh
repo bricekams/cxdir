0299daf2efade28abcf76cfbefbde6278c0198d3
Jotter Kain <jotterkain@gmail.com>
2022-12-15 02:25:04 +0100

    added some known create command errors

--- errors.sh
+++ errors.sh
@@ -6,10 +6,6 @@ function pathx__error_invalid_option () {
     echo -e $red"pathx:"$defaultColor "\"$1\"" "is an invalid option"
 }
 
-function pathx__tips_show_1 () {    
-    echo -e $blue"Tips:"$defaultColor "Use"$cyan "pathx create [shortcut name]"$defaultColor "to create a new shortcut pointing to the current directory"
-}
-
 function pathx__error_path_not_provided () {
     echo -e $red"pathx:"$defaultColor "When using -p or --path, a valid path sould be given"
     see_help_i_create
@@ -32,3 +28,18 @@ function pathx__error_no_command(){
     echo "pathx: A command must be given"
     see_help_i
 }
+
+function pathx__error_dir_inexistant () {
+    echo -e $red"pathx:"$defaultColor "The given directory does not exist"
+}
+
+function pathx__error_bad_format () {
+    echo -e $red"pathx invalid format:"$defaultColor $1
+    see_help_i_create
+}
+
+function pathx__path_already_exist () {
+    echo -e $red"pathx:"$defaultColor "A shortcut to this path already exist!"
+    echo -e $blue"Tips:"$defaultColor "Use$cyan pathx list"$defaultColor to see the saved path
+}
+
