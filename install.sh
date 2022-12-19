6af6f98ebe51e4ab40955936e996c23d8792232a
Jotter Kain <jotterkain@gmail.com>
2022-12-19 12:32:58 +0100

    remove .git in ~/.cxdir

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir ~/.cxdir
-rm -rf .git .gitignore
 touch saved.csv
 echo "Date,Shortcut,Path" >> saved.csv
 cp -r . ~/.cxdir
+rm -rf ~/.cxdir/.git ~/.cxdir/.gitignore
 echo 'export PATH="$PATH":"$HOME/.cxdir"' >> ~/.bashrc
 exit 0
